ee0c2e17af4297741b0e1055a3c5bcb5ad909f6d
Sebastian <56087724+Zebzab7@users.noreply.github.com>
2025-11-02 18:24:48 +0100

    basic dashboard

--- resources/js/components/ui/widget.tsx
+++ resources/js/components/ui/widget.tsx
@@ -1,7 +1,46 @@
-export function Widget() {
+import { CardContent } from "./card"
+
+export function Widget({ className, ...props }: React.ComponentProps<"div">) {
     return (
-        <div>
-            <img className="rounded-3xl" src="/images/kitty.jpeg"></img>
+        <div 
+            data-slot="widget"
+            {...props}/>
+    )
+}
+
+
+type WidgetContentProps = {
+  src: string
+  alt?: string
+} & React.ComponentProps<"img">
+
+export function WidgetContent({src, alt, ...props}: WidgetContentProps) {
+    return <img 
+        src={src} 
+        alt={alt}
+        {...props}/>
+}
+
+// export function WidgetOverlay({}){
+    
+// }
+
+// export function WidgetOverlay({}){
+    
+// }
+
+type WidgetGalleryProps = React.ComponentProps<typeof Widget> & {
+  images: string[];
+};
+
+export function WidgetGallery({images, ...props}: WidgetGalleryProps) {
+    return (
+        <div className="m-6 gap-6 md:columns-2 lg:columns-3">
+            {images.map((src, i) => (
+                <Widget key={src} {...props}>
+                    <WidgetContent className="rounded-xl" src={src} alt={`Image ${i + 1}`} />
+                </Widget>
+            ))}
         </div>
     )
 }
--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,8 +1,14 @@
-import { Widget } from '@/components/ui/widget';
+import { WidgetGallery } from '@/components/ui/widget';
 import AppLayout from '@/layouts/app-layout';
 import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
+
+const images = [
+    '/images/examples/Daylio.jpg',
+    '/images/examples/frog.jpg',
+    '/images/examples/kitty.jpeg',
+];
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -15,11 +21,28 @@ export default function Dashboard() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
-            <div className="m-4 grid gap-4 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-                <Widget />
-                <Widget />
-                <Widget />
-            </div>
+            <WidgetGallery images={images} className="py-4" />
+            {/* <div className="m-4 grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3"> */}
+            {/* <div className="m-0 gap-6 md:columns-2 lg:columns-3">
+                <Widget className="py-3">
+                    <img
+                        className="rounded-3xl"
+                        src="/images/examples/Daylio.jpg"
+                    ></img>
+                </Widget>
+                <Widget className="py-3">
+                    <img
+                        className="rounded-3xl"
+                        src="/images/examples/frog.jpg"
+                    ></img>
+                </Widget>
+                <Widget className="py-3">
+                    <img
+                        className="rounded-3xl"
+                        src="/images/examples/kitty.jpeg"
+                    ></img>
+                </Widget>
+            </div> */}
         </AppLayout>
     );
 }
